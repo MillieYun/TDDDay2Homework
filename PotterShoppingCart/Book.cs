470a0d5fce35597806826465a11e5480e2213f80
Millie Ho <millie.yun@gmail.com>
2016-10-23 10:56:07 +0800

    Implement: summarizing price with no any discount.

---
+++ PotterShoppingCart/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace PotterShoppingCart
+{
+    public class Book
+    {
+        /// <summary>
+        /// 書本ID
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 售價
+        /// </summary>
+        public int Price { get; set; }
+
+        /// <summary>
+        /// 購買數量
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+        }
+
+        /// <summary>
+        /// 計算購買金額
+        /// </summary>
+        /// <param name="books">書藉資料</param>
+        /// <returns></returns>
+        public int GetSum(List<Book> books)
+        {
+            
+            return books.Sum(b => b.Price);
+        }
+    }
+}
